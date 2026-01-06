1145df97127e2c25153b9e8425f5aeaa9a594458
ivanjohannes <ivanjohannes14@gmail.com>
2026-01-06 12:38:50 +0200

    Small change

--- admin/src/lib/server/config.js
+++ admin/src/lib/server/config.js
@@ -3,7 +3,7 @@ import { env as publicEnv } from '$env/dynamic/public';
 
 export default {
 	es_api: {
-		admin_client_api_key: privateEnv.ES_ADMIN_CLIENT_API_KEY || 'es_default_key',
+		admin_client_api_key: privateEnv.ES_CLIENT_API_KEY || 'es_default_key',
 		url: publicEnv.PUBLIC_ES_API_URL
 	}
 };
